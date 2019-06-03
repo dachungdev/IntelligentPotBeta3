65d80031ea144d3e960ed2af762dadf99e2b654a
DACHUNG <ganguochang@hotmail.com>
2019-06-03 23:32:26 +0800

    second commit 190603

--- app/src/main/java/com/example/intelligentpotbeta3/LoginActivity.java
+++ app/src/main/java/com/example/intelligentpotbeta3/LoginActivity.java
@@ -3,16 +3,37 @@ package com.example.intelligentpotbeta3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * DACHUNG
  * 登录界面
  */
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button launchRegiterBtn;
+    private Button launchLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        launchRegiterBtn = (Button) findViewById(R.id.launch_register_btn);
+        launchLoginBtn = (Button) findViewById(R.id.launch_login_btn);
+        launchRegiterBtn.setOnClickListener(this);
+        launchLoginBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.launch_register_btn:
+                Laucher.LaunchActivity(MyApplication.getContext(),MainActivity.class);
+                break;
+            case R.id.launch_login_btn:
+                Laucher.LaunchActivity(MyApplication.getContext(),MainActivity.class);
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/intelligentpotbeta3/MainActivity.java
+++ app/src/main/java/com/example/intelligentpotbeta3/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.intelligentpotbeta3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *DACHUNG
+ * APP主界面
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
     }
 }
